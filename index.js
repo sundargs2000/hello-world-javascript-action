e47e2042c99215cdd32182e5ef4743571b9abaf7
sundargs2000 <sundargs2000@gmail.com>
2020-06-04 19:42:49 +0530

    yelo

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const github = require('@actions/github');
 try {
   const nameToGreet = core.getInput('name');
   console.log(`Hello ${nameToGreet}!`);
-  op = "Result = "+nameToGreet+"-success";
-  core.setOutput('status',op);
+  op = `Result = ${nameToGreet}-success`; 
+  console.log(op); 
+  core.setOutput('status',String(op));
 } catch (error) {
   core.setFailed(error.message);
 }
