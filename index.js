035c40d5193136399abaaa38ef80455422ebcfc1
sundargs2000 <sundargs2000@gmail.com>
2020-06-04 18:49:23 +0530

    My edits

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('name');
   console.log(`Hello ${nameToGreet}!`);
   core.setOutput(nameToGreet+"-success");
