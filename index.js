29f1bb1df2f68052d0cc4430fb806f756dab2951
sundargs2000 <sundargs2000@gmail.com>
2020-06-06 00:28:29 +0530

    npm install

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
+  await exec.exec('npm install');
   const nameToGreet = core.getInput('name');
   console.log(`Hello ${nameToGreet}!`);
   op = `Result = ${nameToGreet}-success`; 
